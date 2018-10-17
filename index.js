fbdf4258183b6f0fb963abeba200698f6f7ff2ba
EVERYDAYHERO2000 <ilyaflare@gmail.com>
2018-10-17 23:56:36 +0300

    fix

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const secretKey = secret.secretKey; //'c6f0158bd0a97f5..........015';
 express()
 .all('/', function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+	res.header("Access-Control-Allow-Headers", "Cache-Control, Pragma, Origin, Authorization, X-Requested-With, Content-Type");
 	res.header("X-Requested-With", "XMLHttpRequest");
 	res.header('Access-Control-Allow-Credentials', true);
   res.header('Access-Control-Allow-Methods', 'GET');
